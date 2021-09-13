03d1982723bd037ee3f3bf38a1f625e466959770
Benoit Figea <60694673+BFH92@users.noreply.github.com>
2021-09-13 11:11:44 +0200

    add belongs_to company_stack to company table

--- db/migrate/20210913084313_create_companies.rb
+++ db/migrate/20210913084313_create_companies.rb
@@ -7,7 +7,7 @@ class CreateCompanies < ActiveRecord::Migration[6.1]
       t.string :github
       t.string :staff_size
       t.boolean :is_it_recruiting
-
+      t.belongs_to :company_category
       t.timestamps
     end
   end
