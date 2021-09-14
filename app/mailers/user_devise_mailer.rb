18b2968b449480963ef0ad8f5b9387456dcf2cf2
Benoit Figea <60694673+BFH92@users.noreply.github.com>
2021-09-14 15:30:20 +0200

    fix conflict with UserMailer

--- app/mailers/user_devise_mailer.rb
+++ app/mailers/user_devise_mailer.rb
@@ -1,7 +1,6 @@
-class UserMailer < Devise::Mailer
+class UserDeviseMailer < Devise::Mailer
   default from:"test@test.com" #"<#{ENV['DEFAULT_FROM_EMAIL']}>" 
   def reset_password_instructions(record, token, opts={})
     super
   end
-
- end
+end
