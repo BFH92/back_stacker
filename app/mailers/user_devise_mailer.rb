773d488f11a12a072cbb464d0fc2ed94285031fe
Benoit Figea <60694673+BFH92@users.noreply.github.com>
2021-09-19 15:37:11 +0200

    fix mailer in production for reset_password

--- app/mailers/user_devise_mailer.rb
+++ app/mailers/user_devise_mailer.rb
@@ -2,10 +2,6 @@ class UserDeviseMailer < Devise::Mailer
   default from:"thpkata2021@gmail.com" #"<#{ENV['DEFAULT_FROM_EMAIL']}>" 
   def reset_password_instructions(record, token, opts={})
     super
-    puts "USERDEVISEMAILER"
-    puts record
-    puts token
-  #  mail(to: record.email, subject: 'password !') 
   end
 end
 
