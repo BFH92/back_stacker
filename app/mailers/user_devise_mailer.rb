4c4e289667bfc9d53a8959290fdb87ef4f30ad3b
Benoit Figea <60694673+BFH92@users.noreply.github.com>
2021-09-19 11:59:08 +0200

    try to fix password email instructions in production

--- app/mailers/user_devise_mailer.rb
+++ app/mailers/user_devise_mailer.rb
@@ -2,5 +2,6 @@ class UserDeviseMailer < Devise::Mailer
   default from:"test@test.com" #"<#{ENV['DEFAULT_FROM_EMAIL']}>" 
   def reset_password_instructions(record, token, opts={})
     super
+    mail(to: @user.email, subject: 'Récupération de mot de passe') 
   end
 end
