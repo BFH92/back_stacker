fad5b3305c481e63058e222b20177a4263a6f9e3
Benoit Figea <60694673+BFH92@users.noreply.github.com>
2021-09-19 10:08:29 +0200

    disable mail delivery in prod to seed properly

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,7 +59,7 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :mailjet
   
-  config.action_mailer.perform_deliveries = true
+  config.action_mailer.perform_deliveries = false
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
