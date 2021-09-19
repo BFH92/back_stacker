fcd02ec0823d7d61472099d949c69e796edd63d1
Benoit Figea <60694673+BFH92@users.noreply.github.com>
2021-09-19 15:55:15 +0200

    fix bug

--- app/models/companies_stack.rb
+++ app/models/companies_stack.rb
@@ -1,5 +1,4 @@
 class CompaniesStack < ApplicationRecord
-
   belongs_to :company
   belongs_to :stack
   validates_uniqueness_of :stack_id, scope: [:stack_id, :company_id] 
--- app/models/users_stack.rb
+++ app/models/users_stack.rb
@@ -2,3 +2,4 @@ class UsersStack < ApplicationRecord
   belongs_to :user
   belongs_to :stack
   validates_uniqueness_of :stack_id, scope: [:stack_id, :user_id] 
+end
