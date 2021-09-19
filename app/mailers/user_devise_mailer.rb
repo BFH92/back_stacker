a6d5628e3c2bb3ca9d1cfe80c7519c96a9a097e3
Benoit Figea <60694673+BFH92@users.noreply.github.com>
2021-09-19 13:37:24 +0200

    fix model

--- app/mailers/user_devise_mailer.rb
+++ app/mailers/user_devise_mailer.rb
@@ -2,7 +2,7 @@ class UserDeviseMailer < Devise::Mailer
   default from:"support@stacker.com" #"<#{ENV['DEFAULT_FROM_EMAIL']}>" 
   def reset_password_instructions(record, token, opts={})
     @token = token
-    mail(to: @user.email, subject: 'Récupération de mot de passe')
+    mail(to:record, subject: 'Récupération de mot de passe')
   end
 end
 
