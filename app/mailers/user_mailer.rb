4940f59926cde7668cb1dc6d664847cb9851f752
shanks <cheikh.kebe.01@gmail.com>
2021-09-22 23:11:48 +0200

    fix url link welcome mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -5,7 +5,7 @@ class UserMailer < ApplicationMailer
     @user = user 
 
     #on définit une variable @url qu'on utilisera dans la view d’e-mail
-    @url  = 'https://stacker-api.herokuapp.com/' 
+    @url  = 'https://stacker-front.herokuapp.com/' 
 
     # c'est cet appel à mail() qui permet d'envoyer l’e-mail en définissant destinataire et sujet.
     mail(to: @user.email, subject: 'Bienvenue chez nous !') 
