70467e219c83f043e2d26163f2060cc1fc32d59b
Benoit Figea <60694673+BFH92@users.noreply.github.com>
2021-09-24 16:57:30 +0200

    delete email for json without login

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -9,25 +9,30 @@ class CompaniesController < ApplicationController
     @staff_size = params[:staff_size]
     @categories = params[:categories]
     @filtered_companies = @completed_companies.filtering(@stacks,@staff_size,@categories,@completed_companies)
-    
     @filtered_companies = @filtered_companies.map{|company|
       category_id = company.company_category_id
       company_category_name = CompanyCategory.find(category_id).name
       company.as_json.merge(stacks: company.stacks, company_stacks: company.companies_stacks, category_name: company_category_name  )
       }
-      render json:  @filtered_companies.first(20)
+      if current_user
+        render json:  @filtered_companies.as_json.first(20)
+      else
+        companies =[]
+        @filtered_companies.first(20).map{|company|
+          companies.push(company.except("email"))
+        }
+        render json:  companies
+      end
   end
 
   def show
     @company_stack = CompaniesStack.where(company_id: @company.id)
     @stacks_names = @company_stack.map{|stack| {name: Stack.find(stack.stack_id).name, company_stack_id: stack.id}}
     render json: @company.as_json.merge(company_stacks: @stacks_names, company_id: @company.id)
-    
   end
 
   def create
     @company = Company.new(company_params)
-
     if @company.save
       render json: @company, status: :created, location: @company
     else
